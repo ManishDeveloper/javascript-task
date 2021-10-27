fc754cde52896a0231b1aba79ea2b15368b72875
amit kumar <amit.kumar@onsumaye.com>
2021-10-27 16:00:48 +0530

    update

--- amit/task-3/custom.js
+++ amit/task-3/custom.js
@@ -1,1 +1,12 @@
 //Write Your JS Code Here
+
+let menuBtn = document.getElementById('menu-icon');
+let menu = document.getElementById('menu-wrap');
+let closeBtn = document.getElementById('close-btn');
+
+menuBtn.addEventListener('click', function(){
+    menu.classList.add('in');
+});
+closeBtn.addEventListener('click', function(){
+    menu.classList.remove('in');
+});
