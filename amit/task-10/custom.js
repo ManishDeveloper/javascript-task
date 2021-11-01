97164127ec746cd84ad2f56d784ca7c67de6e720
amit kumar <amit.kumar@onsumaye.com>
2021-11-01 23:33:24 +0530

    update

---
+++ amit/task-10/custom.js
@@ -0,0 +1,14 @@
+//Write Your JS Code Here
+
+var myVar = setInterval(myFunction, 1000);
+    
+var word = 'w3resource';
+var newArray = word.split('');
+
+function myFunction() {
+    var letter = newArray.pop();
+    var newWord = newArray.unshift(letter);
+    var theWord = newArray.join('');
+    //console.log(theWord);
+    document.getElementById('rotate').innerHTML = theWord;
+}
