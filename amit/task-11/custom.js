70bc7ece016b8b481d48efb40e250565d946ce46
amit kumar <amit.kumar@onsumaye.com>
2021-11-03 23:34:45 +0530

    update

---
+++ amit/task-11/custom.js
@@ -0,0 +1,14 @@
+//Write Your JS Code Here
+
+function check_leapyear(){
+    var year;
+
+    year = document.getElementById("year").value;
+
+    if( (0 == year % 4) && (0 != year % 100) || (0 == year % 400) ){
+        alert(year+" is a leap year");  
+    }
+    else{
+        alert(year+" is not a leap year");  
+    }
+}
